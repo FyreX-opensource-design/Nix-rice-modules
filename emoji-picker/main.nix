a2ffcd51fa23a6e6f305be76eacf61336266d8aa
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-09-22 12:31:01 -0500

    Update main.nix

--- emoji-picker/main.nix
+++ emoji-picker/main.nix
@@ -5,5 +5,11 @@ pkgs.mkShell {
     pkgs.wofi
     pkgs.dotool
     pkgs.wl-clipboard
+    pkgs.bemoji
   ];
+  shellHook = ''
+    export BEMOJI_DB_LOCATION=./data/bemoji
+    export BEMOJI_HISTORY_LOCATION=./data/bemoji
+    export BEMOJI_TYPE_CMD="dotool type"
+  '';
 }
