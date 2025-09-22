b5e7ea1bd4490b1209a0fcf23f14d4efdc22b65e
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-09-22 12:34:03 -0500

    Update main.sh

--- waybar/main.sh
+++ waybar/main.sh
@@ -1,1 +1,1 @@
-nix-shell ./main.nix --command waybar -c ./config/waybar/config -s ./config/waybar/style.css
+nix-shell ./main.nix --command "waybar -c ./config/waybar/config -s ./config/waybar/style.css"
